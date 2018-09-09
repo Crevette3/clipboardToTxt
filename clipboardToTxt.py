574b27d6619667f73bafd363af1b712331b7d84c
Evan Kivolowitz <ekivolowitz@wisc.edu>
2018-09-08 22:18:09 -0700

    Added \n$\n between entries

--- clipboardToTxt.py
+++ clipboardToTxt.py
@@ -1,18 +1,28 @@
 #! python3
 #https://www.youtube.com/channel/UCsZs-p37pse_4UXG-alR47w   (My channel where I explain this code and demonstrate it working)
 # clipboardToTxt.py - Pastes what you copy into a text file
-import time
-import pyperclip
-from contextlib import redirect_stdout
-clipboard = ''
-while 1 != 2:
-    time.sleep(.1) #with this checks ~10 times a second, without it checks ~600 times a second
-    if clipboard != pyperclip.paste():
-        clipboard = pyperclip.paste()
-        with open('clipboard.txt', 'a') as f:
-            with redirect_stdout(f):
-                print(clipboard + '\n')
 
+import time
+import sys
+import pyperclip
+
+# By preloading last_paste with the existing clipboard we avoid saving
+# something from the clipboard that was put there when this tool was not
+# running.  That way you can safely kill it when copying a password and
+# restart it afterwards and it will not steal your sensitive paste.
+last_paste = pyperclip.paste().strip()
+
+while True:
+    time.sleep(0.1)
+    paste = pyperclip.paste().strip()
+    if paste != last_paste:
+        try:
+            with open('clipboard.txt', 'a') as f:
+                f.write('{}\n$\n'.format(paste))
+                last_paste = paste
+        except Exception as e:
+            sys.stderr.write("Error: {}".format(e))
+            break
 
 
 
