b02b83027f1c0880b21a5c5f84e7f39efa58bfc1
Josh Pullen <bolapara@gmail.com>
2018-09-08 23:01:27 -0500

    Update clipboardToTxt.py
    Make more Pythonic.  Handle errors.

--- clipboardToTxt.py
+++ clipboardToTxt.py
@@ -1,18 +1,21 @@
 #! python3
 #https://www.youtube.com/channel/UCsZs-p37pse_4UXG-alR47w   (My channel where I explain this code and demonstrate it working)
-# clipboardToTxt.py - Pastes what you copy into a text file
+# clipboardToTxt.py - Pastes what you copy into a text filefrom 
+
 import time
+import sys
 import pyperclip
-from contextlib import redirect_stdout
-clipboard = ''
-while 1 != 2:
-    time.sleep(.1) #with this checks ~10 times a second, without it checks ~600 times a second
-    if clipboard != pyperclip.paste():
-        clipboard = pyperclip.paste()
-        with open('clipboard.txt', 'a') as f:
-            with redirect_stdout(f):
-                print(clipboard + '\n')
 
+last_paste = ''
 
-
-
+while True:
+    time.sleep(0.1)
+    paste = pyperclip.paste().strip()
+    if paste != last_paste:
+        try:
+            with open('clipboard.txt', 'a') as f:
+                f.write('{}\n'.format(paste))
+                last_paste = paste
+        except Exception as e:
+            sys.stderr.write("Error: {}".format(e))
+            break
